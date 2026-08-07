ab2ee1b5257efcf2275b2f65325909e3acb884dc
Joao Pedro <joaopedroarrudabarbosa@gmail.com>
2026-08-07 14:13:42 -0300

    feat: formulario

--- Alura/src/App.js
+++ Alura/src/App.js
@@ -1,13 +1,11 @@
 import Banner from './componentes/Banner/Banner'
-import CampoTexto from './componentes/CampoTexto';
+import Formulario from './componentes/Formulario/index'
 
-function App() {
+function App () {
   return (
     <div className="App">
     <Banner />
-    <CampoTexto label="Nome" placeholder="Digite seu nome"/>
-    <CampoTexto label="Cargo" placeholder="Digite seu cargo"/>
-    <CampoTexto label="Imagem" placeholder="Digite seu endereço da imagem"/>
+    <Formulario />
     </div>
   );
 }
