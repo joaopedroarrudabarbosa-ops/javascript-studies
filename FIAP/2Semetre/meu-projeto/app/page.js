aae1b7fad772259e7c6286e58250f4037de2c517
João Pedro <joaopedroarrudabarbosa@gmail.com>
2026-09-16 14:44:06 -0300

    fix: folder organization

--- FIAP/2Semetre/meu-projeto/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home () {
-  return (
-    <main>
-      <h1>Eae diogo cachorro!</h1>
-    </main>
-  )
-}
--- FIAP/2Semetre/script.js
+++
@@ -1,8 +0,0 @@
-function App() {
-    return React.createElement('h1', null, 'Hello World sem build');
-}
-
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById('root')
-);
